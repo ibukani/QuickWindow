2061c6e0808e21526e453ea70ce5947dacfeea9c
ibukani <ibukani2056@gmail.com>
2023-11-25 16:46:40 +0900

    add InputManager

--- src-tauri/src/hotkey.rs
+++ src-tauri/src/hotkey.rs
@@ -1,13 +1,15 @@
-pub struct InputManager {
-    hotkeys: vec![],
-}
+use inputbot::handle_input_events;
+
+pub struct InputManager {}
 
 impl InputManager {
-    pub fn new(&self) -> Self {
-        return Self;
+    pub fn new() -> Self {
+        return Self {};
     }
 
-    pub fn init(&self) {
+    pub fn init(&self) {        
+
+        handle_input_events();
         // initialize hoykey
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod hotkey;
+
+use hotkey::InputManager;
 use tauri::SystemTray;
 
 fn main() {
+    let manager = InputManager::new();
+    manager.init();
+
     let system_tray = SystemTray::new();
 
     tauri::Builder::default()
