eec58aa422ab5fb92d8bf1c2991f757bb5fac517
ibukani <ibukani2056@gmail.com>
2023-11-26 01:19:10 +0900

    add InputManager init function

--- src-tauri/src/hotkey.rs
+++ src-tauri/src/hotkey.rs
@@ -1,4 +1,8 @@
-use inputbot::handle_input_events;
+use inputbot::KeybdKey::*;
+use std::{
+    sync::{Arc, Mutex},
+    time::Instant,
+};
 
 pub struct InputManager {}
 
@@ -7,9 +11,27 @@ impl InputManager {
         return Self {};
     }
 
-    pub fn init(&self) {        
+    pub fn init(&self) {
+        // example test code
+        let mut timestamp = Arc::new(Mutex::new(Instant::now()));
+        let mut process = Arc::new(Mutex::new(0 as u8));
 
-        handle_input_events();
-        // initialize hoykey
+        RShiftKey.bind(move || {
+            let mut process_lock = process.lock().unwrap();
+            let mut timestamp_lock = timestamp.lock().unwrap();
+
+            if *process_lock == 0 {
+                *process_lock = 1;
+                *timestamp_lock = Instant::now();
+            } else if *process_lock == 1 {
+                *process_lock = 0;
+
+                if timestamp_lock.elapsed().as_secs() < 1 {
+                    // macrgo function
+                }
+            }
+        });
+
+        inputbot::handle_input_events();
     }
 }
