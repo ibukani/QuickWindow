c25a547012000966c08370258a7f3e6937194e04
ibukani <ibukani2056@gmail.com>
2023-12-07 16:16:46 +0900

    add systemtray item and hide window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,17 +5,50 @@ mod function;
 mod hotkey;
 
 use hotkey::InputManager;
-use tauri::SystemTray;
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayMenu};
 
 fn main() {
     println!("build ing");
     let mut manager = InputManager::new();
     manager.init();
 
-    let system_tray = SystemTray::new();
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+    let tray_menu = SystemTrayMenu::new().add_item(quit);
+
+    let system_tray = SystemTray::new().with_menu(tray_menu);
 
     tauri::Builder::default()
+        .setup(|app| {
+            app.get_window("main").unwrap().hide().unwrap();
+            Ok(())
+        })
         .system_tray(system_tray)
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
+        .on_system_tray_event(|app, event| match event {
+            tauri::SystemTrayEvent::LeftClick { .. } => {
+                let window = app.get_window("main").unwrap();
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            }
+            tauri::SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
